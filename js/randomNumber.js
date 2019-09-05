c7693d07d36d44b97b7746c5f6a664e81af7ffb9
Amilcar34 <alberino-l@hotmail.com>
2019-09-05 13:00:47 -0300

    fix while

--- js/randomNumber.js
+++ js/randomNumber.js
@@ -1,7 +1,7 @@
 function setResultRandom(cantGrups){
     for (let index = 0; index < cantGrups; index++) {
         let resutl = new Set();
-        while (resutl.size == 8) {
+        while (resutl.size != 8) {
             resutl.add(Math.floor((Math.random() * 10) + 1));
         }
         document.getElementById("resultRandomDiv").innerText(resutl)
