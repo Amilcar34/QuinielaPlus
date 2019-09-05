88df9944053525e8a5a4c0f4d77b06c44ae0fa20
Amilcar Alberino <alberino-l@hotmail.com>
2019-09-05 12:43:26 -0300

    first commit

---
+++ js/randomNumber.js
@@ -0,0 +1,9 @@
+function setResultRandom(cantGrups){
+    for (let index = 0; index < cantGrups; index++) {
+        let resutl = new Set();
+        while (resutl.size == 8) {
+            resutl.add(Math.floor((Math.random() * 10) + 1));
+        }
+        document.getElementById("resultRandomDiv").innerHTML(resutl)
+    }
+}
