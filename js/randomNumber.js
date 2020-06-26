de84704c9acf533df193ae11d6af35b8f7c727fd
Amilcar Alberino <alberino-l@hotmail.com>
2020-06-26 19:14:37 -0300

    delete rechapcha

--- js/randomNumber.js
+++ js/randomNumber.js
@@ -2,9 +2,9 @@ function setResultRandom(cantGrups){
     document.getElementById("resultRandomUl").innerHTML = "";
     for (let index = 0; index < cantGrups; index++) {
         let resutl = new Set();
-        while (resutl.size != 8) {
-            resutl.add(Math.floor((Math.random() * 10) + 1));
-        }
+        while (resutl.size != 8) 
+            resutl.add(Math.floor((Math.random() * 100) + 1));
+        
         let resultValue = "";
         for (const element of resutl.entries()) 
             resultValue += element[0] + " - ";            
